b5271134abda25551ef75ea140897b3ea6896439
Dylan Klett <dylanzklett@gmail.com>
2015-12-11 13:53:10 -0500

    Inserted JS functions to display and hide lightbox on click. Added escape x button and styled in CSS.

--- lightbox.js
+++ lightbox.js
@@ -1,8 +1,15 @@
 $(document).ready(
 function(){
 	$(".lightbox_link").on("click", function(){
-		console.log("You clicked me!")
-	})
+		if($('.outer_lightbox').css("display") == "none"){
+			$('.outer_lightbox').show();
+	}
+	});
+	$(".exit").on("click", function(){
+		if($('.outer_lightbox').css("display") == "block"){
+			$('.outer_lightbox').hide();
+		}
+	});
 
 
 
